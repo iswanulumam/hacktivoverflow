766470468874f1d52784130309d1400d2f487389
Iswanul Umam <umam@sepulsa.com>
2018-05-17 11:24:38 +0700

    initial commit

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+require('dotenv').config()
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', index);
 app.use('/users', users);
