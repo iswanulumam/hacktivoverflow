55928a675bd8305c42910d98863644020fb11374
Iswanul Umam <umam@sepulsa.com>
2018-05-19 16:47:33 +0700

    check env var

--- server/lib/connection.js
+++ server/lib/connection.js
@@ -4,6 +4,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI, function (err) {
   if (err) return console.log('failed connect to db');
   console.log('<3 i love coding <3');
+  console.log(process.env.MONGODB_URI);
 });
 
 module.exports = { mongoose };
