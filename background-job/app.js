253d34bd339704be74af17f3c583a5705a2dc9d6
Iswanul Umam <umam@sepulsa.com>
2018-05-22 23:08:09 +0700

    change interval

--- background-job/app.js
+++ background-job/app.js
@@ -10,16 +10,17 @@ const baseUrl = `http://api.hacktivoverflow.iamsuperpowers.com`
 const config = {
   telegramToken: process.env.TELEGRAM_TOKEN,
   groupId: -260568771,
+  interval: '*/30 * * * *'
 }
 
-let job = new CronJob('*/1 * * * *', function() {
+let job = new CronJob(config.interval, function() {
   console.log(`cron execute at: ${new Date()}`)
   axios.get(`${baseUrl}/api/questions`)
   .then((response) => {
     let questions = response.data.data
     let total = QuestionHasNoAnswerYet(questions)
     if (total > 0) {
-      let data = `Message from HacktivoverflowBot bot.\nMany question has not answered yet. total questions is (${total}) `
+      let data = `Message from HacktivoverflowBot bot.\nMany question has not answered yet. total questions is (${total}).\n${new Date()}`
       channel.send(config.telegramToken, config.groupId, data)
     }
   }).catch((e) => {
